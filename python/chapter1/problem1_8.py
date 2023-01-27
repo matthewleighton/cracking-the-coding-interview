309aec6c54222ef3871d0cf58dfc6f2a2eb888e8
Matthew <matthewrleighton@gmail.com>
2023-01-27 19:49:59 +0100

    Python: Chapter 1, Q8 (updates)

--- python/chapter1/problem1_8.py
+++ python/chapter1/problem1_8.py
@@ -1,19 +1,44 @@
 def zero_matrix(matrix):
-	zero_columns, zero_rows = [], []
+	# Record whether the first row/column contains a zero.
+	zero_in_first_column = check_zero_in_first_column(matrix)
+	zero_in_first_row = check_zero_in_first_row(matrix)
 
-	for col in range(len(matrix[0])):
-		for row in range(len(matrix)):
+	# We now use the first row and column to record which rows/columns contain a zero.
+	for col in range(1, len(matrix[0])):
+		for row in range(1, len(matrix)):
 			if matrix[row][col] == 0:
-				zero_columns.append(col)
-				zero_rows.append(row)
+				matrix[0][col] = 0
+				matrix[row][0] = 0
 
-	for col in zero_columns:
-		nullify_column(matrix, col)
+	# Then use those values in the first rows/columns to zero the rest of the matrix.
+	for row in range(1, len(matrix)):
+		if matrix[row][0] == 0:
+			nullify_row(matrix, row)
+
+	for col in range(1, len(matrix[0])):
+		if matrix[0][col] == 0:
+			nullify_column(matrix, col)
+
+	# Finally, we use the booleans we recorded at the start to zero the first row/column if necessary.
+	if zero_in_first_column:
+		nullify_column(matrix, 0)
+
+	if zero_in_first_row:
+		nullify_row(matrix, 0)
 	
-	for row in zero_rows:
-		nullify_row(matrix, row)
-
 	return matrix
+
+def check_zero_in_first_column(matrix):
+	for i in range(len(matrix)):
+		if matrix[i][0] == 0:
+			return True
+	return False
+
+def check_zero_in_first_row(matrix):
+	for i in range(len(matrix[0])):
+		if matrix[0][i] == 0:
+			return True
+	return False
 
 def nullify_column(matrix, col):
 	for row in range(len(matrix)):
--- python/chapter1/test_problem1_8.py
+++ python/chapter1/test_problem1_8.py
@@ -1,7 +1,50 @@
 import unittest
-from problem1_8 import zero_matrix
+from problem1_8 import zero_matrix, check_zero_in_first_column, check_zero_in_first_row
 
 class TestProblem8(unittest.TestCase):
+
+	def test_check_zero_in_first_row_correctly_true(self):
+		matrix = [
+			[1, 0, 3],
+			[4, 5, 6],
+			[7, 8, 9]
+		]
+
+		result = check_zero_in_first_row(matrix)
+		self.assertTrue(result)
+
+	def test_check_zero_in_first_row_correctly_false(self):
+		matrix = [
+			[1, 2, 3],
+			[0, 5, 6],
+			[7, 8, 9]
+		]
+
+		result = check_zero_in_first_row(matrix)
+		self.assertFalse(result)
+
+	
+	def test_check_zero_in_first_column_correctly_true(self):
+		matrix = [
+			[1, 2, 3],
+			[0, 5, 6],
+			[7, 8, 9]
+		]
+
+		result = check_zero_in_first_column(matrix)
+		self.assertTrue(result)
+
+	def test_check_zero_in_first_column_correctly_false(self):
+		matrix = [
+			[1, 2, 3],
+			[4, 5, 6],
+			[7, 8, 9]
+		]
+
+		result = check_zero_in_first_column(matrix)
+		self.assertFalse(result)
+
+	
 
 	def test_returns_original_matrix_if_no_elements_are_zero(self):
 		input_matrix = [
@@ -19,7 +62,6 @@ class TestProblem8(unittest.TestCase):
 		result = zero_matrix(input_matrix)
 		
 		self.assertEqual(result, input_matrix)
-
 
 	def test_works_when_single_zero_element(self):
 		input_matrix = [
@@ -54,6 +96,23 @@ class TestProblem8(unittest.TestCase):
 		result = zero_matrix(input_matrix)
 
 		self.assertEqual(result, expected_output)
+
+	def test_works_with_rectangle_matrix_zero_in_first_column(self):
+		input_matrix = [
+			[1, 2],
+			[0, 4],
+			[5, 6]
+		]
+
+		excepted_output = [
+			[0, 2],
+			[0, 0],
+			[0, 6]
+		]
+
+		result = zero_matrix(input_matrix)
+
+		self.assertEqual(result, excepted_output)
 
 
 if __name__ == '__main__':
