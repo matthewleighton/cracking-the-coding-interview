92b3122c58c7c78734b34a4e0d67e231fee3cd0d
Matthew <matthewrleighton@gmail.com>
2023-01-29 11:28:03 +0100

    Python: Chapter 2, Q4 (updates)

--- python/chapter2/problem2_4.py
+++ python/chapter2/problem2_4.py
@@ -1,23 +1,36 @@
+from LinkedList import Node
+
+# Sort a list, such that all elements less than the partition_value are on the left,
+# while all elements greater are on the right.
+# Aside for this, we do not care about value ordering.
 def partition(linked_list, partition_value):
 	current = linked_list.head
-	runner = linked_list.head
+	end_node = linked_list.head
 
+	# We get the list's last node, and count the length at the same time.
 	length = 0
-
-	while runner.next is not None:
-		runner = runner.next
+	while end_node.next is not None:
+		end_node = end_node.next
 		length += 1
 
 	i = 0
-	# while current.next is not None:
 	while True:
+
+		# The next element is greater than (or equal to) the partition_value, so we move it to the end.
 		if current.next.data >= partition_value:
-			linked_list.append(current.next.data)
+		
+			# The function linked_list.append needs to walk through the whole list each time.
+			# But we already have the end_node. So instead we just change that node's "next" value, without doing the walk.
+			end_node.next = Node(current.next.data)
+			end_node = end_node.next
 			current.next = current.next.next
-		else:
+		
+		else: # Otherwise, just continue walking to the end of the list.
 			current = current.next
 
 		i += 1
 
+		# We'll never actually reach the end of the list, since we'll get stuck in a loop of moving elements back.
+		# So instead we just stop once we've moved as many elements as are in the list.
 		if i >= length:
 			break
